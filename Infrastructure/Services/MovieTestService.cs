976cf24ebb7e262ed35b4a9217f25191f9d74f9f
Will <billlach1995@yahoo.com>
2021-10-20 12:04:56 -0400

    Oops. Made a mistake and built solution on 4.7.2 instead of 5.0.x. Need to remake project, but pushing code anyways to make the rebuilding process easier.

---
+++ Infrastructure/Services/MovieTestService.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Models;
+using ApplicationCore.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    class MovieTestService : IMovieService
+    {
+        public List<MovieCardResponseModel> GetTop30RevenueMovies()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MovieAppProject/Controllers/HomeController.cs
+++ MovieAppProject/Controllers/HomeController.cs
@@ -1,22 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MovieAppProject.Models;
+using Infrastructure.Services;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ApplicationCore.ServiceInterfaces;
 
 namespace MovieAppProject.Controllers
 {
     public class HomeController : Controller
     {
-
+        private IMovieService _movieService;
+        public HomeController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+        //https://localhost/home/index
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            //call movie service class to get list of movie card models
+            var movieCards = _movieService.GetTop30RevenueMovies();
+            //passing data from controller to view, strongly typed models (preferred way)
+            //ViewBag AND ViewData
+            return View(movieCards);
         }
 
+        //https://localhost/home/privacy
+        [HttpGet]
         public IActionResult Privacy()
         {
             return View();
